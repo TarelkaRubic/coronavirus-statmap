4b209cf1b2daef2740c5f47075ff9cf2c40c1401
Diwax1 <andreysmith2018@gmail.com>
2021-11-27 19:33:09 +0300

    map

--- src/views/covid.vue
+++ src/views/covid.vue
@@ -1,10 +1,11 @@
 <template>
   <div>  
+     <a class="bg-blue-400  text-white rounded p-2 mt-9 flex flex-col align-center justify-center text-center padding-10" href="/">Статистика</a>
     <div id="svgMap"></div>
   </div>
 </template>
 
-<script src="@/main1.js">
+<script src="@/map.js">
 
 </script>
 
